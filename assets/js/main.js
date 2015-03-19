3acc4820f3f47fb3c7a6ac2ee021a40f0ed0a41c
Nabeel <nabeel@qburst.com>
2015-03-19 18:16:27 +0530

    functionalities added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-var calculatorModel = function calculator(){
+var calculatorModel = function calculatorModel(){
 	var index=this;
 
 	// array of commands
@@ -15,7 +15,8 @@ var calculatorModel = function calculator(){
 			{val:'7'},
 			{val:'8'},
 			{val:'9'},
-			{val:'0'}
+			{val:'0'},
+			{val:'.'}
 	];
 
 	//result view
@@ -36,9 +37,41 @@ var calculatorModel = function calculator(){
 		index.result(eval(index.result()));
 	}
 
+	//trignometry
+	index.sin = function(){
+		var x = Math.sin(index.result());
+		index.result(x);
+	}
+	index.cos = function(){
+		var y = Math.cos(index.result());
+		index.result(y);
+	}
+	index.tan = function(){
+		var z = Math.tan(index.result());
+		index.result(z);
+	}
+
+
 	//clear Result
 	index.clearResult = function(){
 		index.result('');
+	}
+
+	//clear last element
+	index.clearLast = function(){
+		var result = index.result();
+		console.log(result);
+		// JSON.stringify(result);
+		var array = [];
+		for(var i in result){                               
+			array.push(result[i]);
+		}
+		array.pop();
+		var str= '';
+		for(var i in array){
+			str+=array[i];
+		}
+		index.result(str);
 	}
 };
 ko.applyBindings(new calculatorModel());
